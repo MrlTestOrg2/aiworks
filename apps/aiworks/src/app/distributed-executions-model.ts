2899a199c2608d2a715686ab865ae8d5e96d33a2
Mark Lindsey <markl@nrwl.io>
2025-06-17 10:05:30 -0400

    added file to compare

---
+++ apps/aiworks/src/app/distributed-executions-model.ts
@@ -0,0 +1,79 @@
+import { MongoDistributedTask } from '@nx-cloud/model-db';
+
+export type DistributedExecutionStatus =
+  | 'NOT_STARTED'
+  | 'IN_PROGRESS'
+  | 'COMPLETED';
+
+export type DteAgentStatus = 'IDLE' | 'ACTIVE' | 'COMPLETED';
+
+/** DTE visualization */
+export interface DteVizAgent<T> {
+  name: string;
+  displayName: string;
+  launchTemplate?: string;
+  status: DteAgentStatus;
+  totalTasksScheduled: number;
+  tasks: T[];
+  inProgressTasks: T[];
+  completedTasks: T[];
+}
+
+export interface DteVizApiAgent extends DteVizAgent<DteVizApiAgentTask> {
+  showOutOfMemoryWarning: boolean;
+}
+
+export interface DteVizViewAgent extends DteVizAgent<DteVizViewAgentTask> {
+  showOutOfMemoryWarning: boolean;
+  totalInProgressTasks?: number;
+}
+
+export interface DteVizDistributedExecution {
+  id: string;
+  command: string;
+  executionStatus: DistributedExecutionStatus;
+  tasks: DteVizApiAgentTask[][];
+}
+
+export interface DteVizApiAgentTask {
+  command: string;
+  executionStatus: MongoDistributedTask['status'];
+  distributedExecutionId: string;
+  taskId: string;
+}
+
+export interface DteVizAgentCompletedTask extends DteVizApiAgentTask {
+  startTime: Date;
+  endTime: Date;
+  duration?: number;
+}
+
+export interface DteVizViewAgentTask extends DteVizApiAgentTask {
+  elementId: string;
+}
+
+/** Run Group agent analysis */
+export interface AgentUtilizationSummary {
+  name: string;
+  displayName: string;
+  launchTemplate?: string;
+  totalTasksScheduled: number;
+  completedTasksCount: number;
+  percentUtilization: number;
+  totalActiveTime: number;
+  activeTimeSlices: Array<AgentUtilizationTimeSlice>;
+}
+
+export interface AgentUtilizationTimeSlice {
+  startTime: string;
+  endTime: string;
+  duration: number;
+  tasks: AgentUtilizationTask[];
+}
+
+interface AgentUtilizationTask {
+  command: string;
+  startTime: string;
+  endTime: string;
+  duration: number;
+}
