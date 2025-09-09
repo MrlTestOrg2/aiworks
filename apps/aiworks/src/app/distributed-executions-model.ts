caa49158c219506eb1f1c384cbe93289a18cb717
Mark Lindsey <markl@nrwl.io>
2025-09-09 11:37:16 -0400

    causing an error

--- apps/aiworks/src/app/distributed-executions-model.ts
+++ apps/aiworks/src/app/distributed-executions-model.ts
@@ -1,9 +1,10 @@
 import { MongoDistributedTask } from '@nx-cloud/model-db';
 
+
 export type DistributedExecutionStatus =
   | 'NOT_STARTED'
   | 'IN_PROGRESS'
-  | 'COMPLETED'
+  | 'COMPLETED
   | 'FAILED';
 
 export type DteAgentStatus = 'IDLE' | 'ACTIVE' | 'COMPLETED';
